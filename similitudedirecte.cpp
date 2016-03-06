141e616fbb22655b66f83508b6f922632222763d
Alexsaphir <alex.game@sfr.fr>
2016-03-06 20:59:03 +0100

    similitude Directe update

--- similitudedirecte.cpp
+++ similitudedirecte.cpp
@@ -7,10 +7,31 @@ SimilitudeDirecte::SimilitudeDirecte() : Application()
 
 SimilitudeDirecte::SimilitudeDirecte(qreal K, qreal theta, QPointF P) :Application()
 {
+	k=K;
 
+	m11 = qCos(theta);
+	m12 = -qSin(theta);
+	m21 = qSin(theta);
+	m22 = qCos(theta);
+
+	v1 = P.x();
+	v2 = P.y();
+
+	Centre.setX(0);
+	Centre.setY(0);
 }
 
 SimilitudeDirecte::SimilitudeDirecte(qreal K, qreal theta, QPointF P, QPointF C) : Application()
 {
+	k=K;
 
+	m11 = qCos(theta);
+	m12 = -qSin(theta);
+	m21 = qSin(theta);
+	m22 = qCos(theta);
+
+	v1 = P.x();
+	v2 = P.y();
+
+	Centre = C;
 }
